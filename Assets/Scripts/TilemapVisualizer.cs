d49ebf4e82913bfde1c968dbe507a04d1b415e3b
Joshua Burt <joshuaadburt@gmail.com>
2022-11-22 15:48:48 -0400

    Generated dungeons now no longer generate with spread out tiles

--- Assets/Scripts/TilemapVisualizer.cs
+++ Assets/Scripts/TilemapVisualizer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -15,6 +16,7 @@ public class TilemapVisualizer : MonoBehaviour
 {
     [SerializeField] private Tilemap floorTilemap, wallTilemap;
     [SerializeField] private TileBase floorTile, wallTop;
+    [SerializeField] private Grid grid;
 
     public void PaintFloorTiles(IEnumerable<Vector2Int> floorPos)
     {
@@ -26,15 +28,17 @@ public class TilemapVisualizer : MonoBehaviour
         foreach (var position in positions)
         {
             PaintSingleTile(tilemap, tile, position);
-        }
+        }      
     }
 
     private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
     {
+        grid.cellSize = new Vector2(1, 1);
         //Create a variable for the tile position, by converting a world position to a cell position
         var tilePosition = tilemap.WorldToCell((Vector3Int)position);
         //Paint the actual tile to that position
         tilemap.SetTile(tilePosition, tile);
+        grid.cellSize = new Vector2(0.32f, 0.32f);
     }
 
     internal void PaintSingleBasicWall(Vector2Int position)
