2e347b33c18cd469f5a9f379f94b12ca8bd872ac
Aditya Sharma <ad437951@dal.ca>
2022-11-22 02:48:48 -0400

    added projectile ability for player that damages enemies

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Border":
+                Destroy(gameObject);
+                break;
+            case "Enemy":
+                collision.gameObject.GetComponent<EnemyControl>().Damage(1);
+                Destroy(gameObject);
+                break;
+        }
+        Destroy(gameObject,0.5f);
+    }
+}
---
+++ Assets/Scripts/SabreHitbox.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SabreHitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyControl>().Damage(1);
+        }
+    }
+}
