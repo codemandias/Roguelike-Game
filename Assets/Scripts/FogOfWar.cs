bd763eb571c933ef4121182f7afd12c6b47be294
Joshua Burt <joshuaadburt@gmail.com>
2022-11-22 00:56:07 -0400

    Created a dynamic mesh that represents the lit up area

--- Assets/Scripts/FogOfWar.cs
+++ Assets/Scripts/FogOfWar.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class FogOfWar : MonoBehaviour {
     public float sightDistance;
     public float angle = 15;
     public const float MIN_ANGLE = 0.1f;
+    public GameObject quad;
 
     void FixedUpdate() {
         int layerMask = 1 << 6;
@@ -14,15 +16,43 @@ public class FogOfWar : MonoBehaviour {
             return;
         }
 
-        for(int i = 0; i < 360/angle; i++) {
-            Vector3 dir = Quaternion.AngleAxis(angle*i, Vector3.forward) * Vector3.right;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, sightDistance, layerMask);
+        int numberOfVertices = (int)Mathf.Floor(360 / angle);
 
-            if(hit.collider != null) {
-                Debug.DrawLine(transform.position, hit.point);
-            } else {
-                Debug.DrawLine(transform.position, transform.position + (dir * sightDistance));
-            }
+        Vector3[] lightVertices = new Vector3[numberOfVertices + 1];
+        int[] lightTriangles = new int[numberOfVertices * 3];
+
+        lightVertices[0] = new Vector2(0, 0);
+
+        for(int i = 1; i < numberOfVertices; i++) {
+            lightVertices[i] = Quaternion.AngleAxis(angle * i, Vector3.forward) * Vector3.right;
+        }
+
+        lightVertices[numberOfVertices] = new Vector2(1, 0);
+
+        lightTriangles[0] = 0;
+        lightTriangles[1] = 1;
+        lightTriangles[2] = 2;
+
+        int sub = 0;
+
+        for(int i = 3; i < lightTriangles.Length; i+=3) {
+            lightTriangles[i] = 0;
+            lightTriangles[i + 1] = lightTriangles[i-1];
+            lightTriangles[i + 2] = i - sub <= numberOfVertices ? i - sub : 1;
+
+            sub+=2;
+        }
+
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = lightVertices;
+        mesh.triangles = lightTriangles;
+
+        quad.GetComponent<MeshFilter>().mesh = mesh;
+
+        for(int i = 0; i < numberOfVertices - 1; i++) {
+            Vector3 dir = Quaternion.AngleAxis(angle*i, Vector3.forward) * Vector3.right;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir.normalized, sightDistance, layerMask);
         }
     }
 }
