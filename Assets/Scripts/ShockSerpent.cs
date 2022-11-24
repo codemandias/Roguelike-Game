3668054c90ce0f34886c56142510be9939538be0
Aditya Sharma <ad437951@dal.ca>
2022-11-24 05:21:39 -0400

    added a knockback on shock trap and fixed a bug where sword colliding was damaging player

--- Assets/Scripts/ShockSerpent.cs
+++ Assets/Scripts/ShockSerpent.cs
@@ -16,12 +16,26 @@ public class ShockSerpent : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Vector3 contactPoint = collision.contacts[0].point;
+            Vector3 center = collision.collider.bounds.center;
+
+            bool right = contactPoint.x > center.x;
+            bool left = contactPoint.x < center.x;
+            bool top = contactPoint.y > center.y;
+            bool bottom = contactPoint.y < center.y;
+
+            if (left) collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.right, ForceMode2D.Impulse);
+            if (right) collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.right, ForceMode2D.Impulse);
+            if (bottom) collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up, ForceMode2D.Impulse);
+            if (top) collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.up, ForceMode2D.Impulse);
             collision.gameObject.GetComponent<PlayerHealth>().shockDamage(1);
         }
     }
+
 
 }
